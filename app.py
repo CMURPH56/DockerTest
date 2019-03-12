b8fae1fac7f38a6e9275f1d23473201ce04aea66
Cam Murphy <CamMurphy195@gmail.com>
2019-03-11 22:38:49 -0500

    created docker file

---
+++ app.py
@@ -0,0 +1,24 @@
+import time
+
+import redis
+from flask import Flask
+
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        return cache.incr('hits')
+ 
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
